07e1b17d2c8698dd600b88b7f73707e764392c20
OusmaneMano <ousmanemanot@gmail.com>
2023-09-12 15:54:28 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _isalpha - checks if letter is lower or upper
+ *
+ * @c: detects alphabets
+ *
+ * Description: use _putchar to print
+ *
+ * Return: 1 if letter otherwise 0
+ *
+ */
+
+int _isalpha(int c)
+{
+	int letter = 0;
+	char alp;
+	char ALP;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		for (ALP = 'A'; ALP <= 'Z'; ALP++)
+		{
+			if (c == alp || c == ALP)
+			{
+				letter = 1;
+			}
+		}
+	}
+	return (letter);
+}
+
