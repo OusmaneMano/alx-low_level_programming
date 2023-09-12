f40799fa5f9e5f4e63e2c91f85fe6624597b7747
OusmaneMano <ousmanemanot@gmail.com>
2023-09-12 09:49:20 -0700

    main.h

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * _putchar - write a character c to stout
+ *
+ * @c: the character to print
+ * 
+ * Return 1 (success)
+ */
+
+int _putchar(char c)
+{
+	return(1, &c, 1);
+}
+
