11d4e6c59bd1b79d0ed142f5c461d2350da5dc5f
OusmaneMano <ousmanemanot@gmail.com>
2023-09-12 15:17:03 -0700

    Correction of task 1 and 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print alphabet - Entry point
+ * print_alphabet - Entry point
  *
  * Return: void method
  */
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- * _islower : write a function that checks for lowercase
+ * _islower - detects lowercase and uppercase
+ *
+ * @c: collect alphabets
+ *
+ * Description: use _putchar to print
  *
  * Return: 1 if c is lowercase else 0
  */
