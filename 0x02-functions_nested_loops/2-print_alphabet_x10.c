4c8fff9687a4869b84cbfa38c4a181437b8d73cf
OusmaneMano <ousmanemanot@gmail.com>
2023-09-12 12:52:26 -0700

    Correction task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,16 @@
 /**
  * print_alphabet_x10 - print 10 times the alphabet in lowercase
  *
- * Return: void 
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 {
 	int i;
+	char al;
 
 	while (i <= 10)
 	{
-		char al;
 
 		for (al = 'a'; al <= 'z'; al++)
 		{
