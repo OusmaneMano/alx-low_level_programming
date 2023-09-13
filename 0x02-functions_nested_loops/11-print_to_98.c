3511aff616211b14cb164ec0b6095e95bc0e1cd6
OusmaneMano <ousmanemanot@gmail.com>
2023-09-13 11:03:34 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from 0 to 98
+ *
+ * @n: number
+ *
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+	for (; i <= 98; i++)
+	{
+		if (n == 98)
+		{
+			_putchar(n);
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(n);
+			_putchar(44);
+			_putchar(32);
+		}
+	}
+	}
+	else
+	{
+	for (; n <= 98; n--)
+	{
+	if (n == 98)
+	{
+	_putchar(n);
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar(n);
+	_putchar(44);
+	_putchar(32);
+	}
+	}
+	}
+}
+
