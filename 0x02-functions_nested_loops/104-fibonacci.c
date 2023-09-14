b5484988749728f265dd1d59df13d3c7bc3b8f43
OusmaneMano <ousmanemanot@gmail.com>
2023-09-13 17:14:02 -0700

    Correction task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,9 +9,10 @@ int main(void)
 	int term1 = 1;
 	int term2 = 2;
 	int next;
+	int i;
 
 	printf("%d, ", term1);
-	while (next <= 98)
+	for (i = 0; i < 98; i++)
 	{
 		printf("%d", term2);
 		next = term1 + term2;
