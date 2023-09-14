61d248e300590fefc0fb2229c0adf79c0a662580
OusmaneMano <ousmanemanot@gmail.com>
2023-09-13 17:55:37 -0700

    Correction task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,18 +8,18 @@ int main(void)
 {
 	unsigned long int term1 = 1;
 	unsigned long int term2 = 2;
-	unsigned long int next;
+	unsigned long int next = 0;
 	int i;
 
-	printf("%lu, ", term1);
 	for (i = 0; i < 98; i++)
 	{
-		printf("%lu", term2);
 		next = term1 + term2;
 		term1 = term2;
 		term2 = next;
+		printf("%lu", term2);
+
 		if (i != 97)
-			printf(", ");
+		printf(", ");
 	}
 	printf("\n");
 	return (0);
