c84041a7afce4802838f7a3f97b6ee322feb810a
OusmaneMano <ousmanemanot@gmail.com>
2023-09-14 19:57:43 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry point
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+	if (x != 50 && x!= 52)
+	_putchar(x);
+	}
+	_putchar('\n');
+}
+
