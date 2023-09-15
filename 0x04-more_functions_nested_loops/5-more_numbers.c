8ec3312e8d1f52dbabdead006d9a6d61127237b7
OusmaneMano <ousmanemanot@gmail.com>
2023-09-15 15:45:15 -0700

    Correction of task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,7 @@
 #include "main.h"
 /**
+ * more_numbers - print 0 to 14 ten times
+ *
  * Return: void method
  */
 
@@ -7,14 +9,17 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+			_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
 		}
-			_putchar('\n');	
-	}
 	_putchar('\n');
+	}
 }
 
