c684508ada2483e758ed9164c4c6d5e904ba8248
OusmaneMano <ousmanemanot@gmail.com>
2023-09-19 15:47:47 -0700

    Corection of task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,18 +11,14 @@ void puts_half(char *str)
 
 	longi = 0;
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		longi++;
-		m = longi / 2;
-	}
+
+	m = longi / 2;
 	if ((m % 2) == 1)
-	{
 		m = (longi + 1) / 2;
+
 	for (i = m; str[i] != '\0'; i++)
-	{
 		_putchar(str[i]);
-	}
-	}
 	_putchar('\n');
 }
 
