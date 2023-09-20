eea2b6298e63bf82894a1810d849596b6a4f0e7b
OusmaneMano <ousmanemanot@gmail.com>
2023-09-20 15:06:23 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * str - changes all lowercase to uppercase
+ * @str: lowe case
+ * Return: uppercase
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
+
