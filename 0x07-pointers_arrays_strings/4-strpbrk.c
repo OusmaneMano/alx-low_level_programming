2ed7c0daf8cded975afd54637feb8931103b6da3
OusmaneMano <ousmanemanot@gmail.com>
2023-09-25 15:41:18 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry
+ * @s: input
+ * @accept: accept
+ * Return: always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+	for (i = 0; accept[i]; i++)
+	{
+		if (*s == accept[i])
+		{
+			return (s);
+		}
+	}
+	s++;
+	}
+	return ('\0');
+}
+
