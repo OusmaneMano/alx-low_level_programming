73231d65063a6b9c4adb370d30517b80b9abe999
OusmaneMano <ousmanemanot@gmail.com>
2023-09-25 16:18:27 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (
