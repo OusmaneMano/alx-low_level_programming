4a7f5b263788e64297eac673a8d63e27af59397e
OusmaneMano <ousmanemanot@gmail.com>
2023-10-04 11:00:45 -0700

    Correction of taask 2

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -12,9 +12,6 @@ int main(void)
     char *s;
 
     s = str_concat("Betty ", "Holberton");
-    s = str_concat("Hello", NULL);
-    s = str_concat(NULL, "Hello");
-    s = str_concat(NULL, NULL);
     if (s == NULL)
     {
         printf("failed\n");
