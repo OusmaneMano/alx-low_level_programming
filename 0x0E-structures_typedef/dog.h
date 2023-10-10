e8bc8af6a2f00a734281c7682f5af8f786f1d10a
OusmaneMano <ousmanemanot@gmail.com>
2023-10-10 13:32:41 -0700

    TAsk 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,8 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-void pring_dog(struct dog *d);
+void print_dog(struct dog *d);
 char *_strcpy(char *dset, char src);
+int _strlen(char *s);
 
 #endif
