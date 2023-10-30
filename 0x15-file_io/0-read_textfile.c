735780bc252c6609a115e46250df48ba49332cf3
OusmaneMano <ousmanemanot@gmail.com>
2023-10-30 13:26:26 -0700

    correct task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX 
+ * read_textfile - reads a text file and prints it to the POSIX
  * @filename: name of the file to read
- * @letters: number of letters 
+ * @letters: number of letters
  * Return: the actual number of letters
  */
 
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	file_d = open(filename, O_RDONLY);
 	if (file_d == -1)
-		return (-1);
+		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
